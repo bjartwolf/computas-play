e79136fceee9337e7e1a676a2ad6fbe4458fdf6a
eih <einarwh@gmail.com>
2013-04-03 15:39:48 +0200

    kittens. spin for firefox. spin 360.

--- play.js
+++ play.js
@@ -1,7 +1,16 @@
 var play = function(){
   console.log('Velkommen til konsollet. Skriv play.spin() og se hva som skjer! Skriv play.help() for hjelp og play.about() for bakgrunnsinfo ');
 };
-play.spin = function () { $("body").css("-webkit-transform", "rotate3d(1,1,0,180deg)")};
+play.spin = function () {
+	var rotated = false;
+	return function() {
+    var rot = rotated ? "0deg" : "180deg"; 
+    rotated = ! rotated;
+	$("body")
+		.css("-webkit-transform", "rotate3d(1,1,0," + rot + ")")
+		.css("-moz-transform", "rotate3d(1,1,0," + rot + ")")
+	}
+}();
 play.help = function () { console.log("Skriv play.spin() og trykk enter")};
 play.about = function () { console.log("Sjekk ut http://blog.bjartwolf.com/?p=552") };
 play.newColor = function () { return "rgb("+parseInt(Math.random()*256)+","+parseInt(Math.random()*256)+","+parseInt(Math.random()*256)+")" };
@@ -15,3 +24,7 @@ play.blinkBackground = function () {
 			complete: function () { play.blinkBackground(); }
 		});
 };
+play.kittens = function() {
+	var img = '<img src="/Documents/Bilder/kittens.jpg" alt="KITTENS!!!"/>';
+	$("#Slider-wrapper").html(img);
+}
