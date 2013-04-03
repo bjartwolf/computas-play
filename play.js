d7d0efd2ebbe25abcb37e30b65837212e2ec9e50
Askild Tønne <askildt@gmail.com>
2013-04-03 15:01:53 +0200

    Blinking background

--- play.js
+++ play.js
@@ -4,3 +4,14 @@ var play = function(){
 play.spin = function () { $("body").css("-webkit-transform", "rotate3d(1,1,0,180deg)")};
 play.help = function () { console.log("Skriv play.spin() og trykk enter")};
 play.about = function () { console.log("Sjekk ut http://blog.bjartwolf.com/?p=552") };
+play.newColor = function () { return "rgb("+parseInt(Math.random()*256)+","+parseInt(Math.random()*256)+","+parseInt(Math.random()*256)+")" };
+play.blinkBackground = function () {
+	$("div").animate(
+		{ 
+			backgroundColor: play.newColor() 
+		},
+		{ 
+			duration: 2000,
+			complete: function () { play.blinkBackground(); }
+		});
+};
