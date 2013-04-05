d7e1f60501c2d1b716a0c6ac6c676975f772f1ab
Askild Tønne <askildt@gmail.com>
2013-04-05 14:23:02 +0200

    Added some shake

--- play.js
+++ play.js
@@ -23,10 +23,9 @@ play.spin = function () {
 play.spin.help = function () {
 	console.log('Vi burde forklare hvordan denne virker...');
 }
-play.help = function () { console.log("Skriv play.spin() og trykk enter. Eller play.kitten(). Eller play.blinkBackground()."); };
+play.help = function () { console.log("Skriv play.spin() og trykk enter. Eller play.kitten(). Eller play.blinkBackground(). Eller play.shake()."); };
 play.about = function () { console.log("Sjekk ut http://blog.bjartwolf.com/?p=552"); };
 play.blinkBackground = function () {
-
     var newColor = function() {
         var f = function() {
             return parseInt(Math.random()*256, 10);
@@ -57,3 +56,71 @@ play.kitten = function() {
 play.kitten.help = function () {
     console.log('Vi burde forklare hvordan denne virker...');
 }
+play.shake = function () {
+    $("body").addClass("animated wobble");
+    var getWobbleCss = function(){/*
+        <style>        
+        .animated {
+            -webkit-animation-duration: 1s;
+               -moz-animation-duration: 1s;
+                 -o-animation-duration: 1s;
+                    animation-duration: 1s;
+            -webkit-animation-iteration-count: infinite;
+               -moz-animation-iteration-count: infinite;
+                 -o-animation-iteration-count: infinite;
+                    animation-iteration-count: infinite;
+            -webkit-animation-fill-mode: both;
+               -moz-animation-fill-mode: both;
+                 -o-animation-fill-mode: both;
+                    animation-fill-mode: both;
+        }
+        @-webkit-keyframes wobble {
+          0% { -webkit-transform: translateX(0%); }
+          15% { -webkit-transform: translateX(-25%) rotate(-5deg); }
+          30% { -webkit-transform: translateX(20%) rotate(3deg); }
+          45% { -webkit-transform: translateX(-15%) rotate(-3deg); }
+          60% { -webkit-transform: translateX(10%) rotate(2deg); }
+          75% { -webkit-transform: translateX(-5%) rotate(-1deg); }
+          100% { -webkit-transform: translateX(0%); }
+        }
+
+        @-moz-keyframes wobble {
+          0% { -moz-transform: translateX(0%); }
+          15% { -moz-transform: translateX(-25%) rotate(-5deg); }
+          30% { -moz-transform: translateX(20%) rotate(3deg); }
+          45% { -moz-transform: translateX(-15%) rotate(-3deg); }
+          60% { -moz-transform: translateX(10%) rotate(2deg); }
+          75% { -moz-transform: translateX(-5%) rotate(-1deg); }
+          100% { -moz-transform: translateX(0%); }
+        }
+
+        @-o-keyframes wobble {
+          0% { -o-transform: translateX(0%); }
+          15% { -o-transform: translateX(-25%) rotate(-5deg); }
+          30% { -o-transform: translateX(20%) rotate(3deg); }
+          45% { -o-transform: translateX(-15%) rotate(-3deg); }
+          60% { -o-transform: translateX(10%) rotate(2deg); }
+          75% { -o-transform: translateX(-5%) rotate(-1deg); }
+          100% { -o-transform: translateX(0%); }
+        }
+
+        @keyframes wobble {
+          0% { transform: translateX(0%); }
+          15% { transform: translateX(-25%) rotate(-5deg); }
+          30% { transform: translateX(20%) rotate(3deg); }
+          45% { transform: translateX(-15%) rotate(-3deg); }
+          60% { transform: translateX(10%) rotate(2deg); }
+          75% { transform: translateX(-5%) rotate(-1deg); }
+          100% { transform: translateX(0%); }
+        }
+
+        .wobble {
+            -webkit-animation-name: wobble;
+            -moz-animation-name: wobble;
+            -o-animation-name: wobble;
+            animation-name: wobble;
+        }
+        </style>
+    */}.toString().slice(14,-3);    
+    $("head").append(getWobbleCss);    
+};
